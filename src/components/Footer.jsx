cc5b09f58f9667913fac4f7fcc91a3ec54972c7c
Filip <orakfilip@gmail.com>
2023-10-14 16:08:25 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import { Button, Typography, Box } from "@mui/material";
+
+export default function Footer() {
+  const handleClick = () => {
+    window.open("https://github.com/Of2211" ,"_blank");
+  };
+
+  return (
+    <Box
+      sx={{
+        p: 1,
+        bgcolor: "#0860cb",
+        color: "white",
+        maxHeight: "50px",
+      }}
+    >
+      <Button
+        className="btn-footer"
+        sx={{
+          border: "3px solid red",
+          ":hover": {
+            transform: "scale(1.1)",
+            transition: "transform 0.1s ease-in-out",
+            bgcolor: "red",
+            color: "white",
+          },
+        }}
+        color="inherit"
+        onClick={handleClick}
+      >
+        GitHub
+      </Button>
+
+      <Typography variant="body2" align="center">
+        Copyright © {new Date().getFullYear()}
+      </Typography>
+    </Box>
+  );
+}
